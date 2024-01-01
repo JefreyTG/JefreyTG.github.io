fbed462f2e3fa7b8161ba0dcfc98b65239e1317a
JefreyTG <yerffej01@gmail.com>
2023-12-31 22:16:38 -0500

    Cuarto commit: se organizo el contenido del componente Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,21 +3,21 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHtml5, faCss3, faJs, faReact } from '@fortawesome/free-brands-svg-icons';
 import './Home.css';
-import myImage from '../images/istockphoto-1856888927-1024x1024.jpg';  // Ajusta la ruta de la imagen según tu estructura de archivos
-
+import mainImage from "../images/istockphoto-1856888927-1024x1024.jpg"
 
 const Home = () => {
   return (
     <div className="home-container">
-      <h1 className="home-title">Hola, Soy Jefrey Ternera</h1>
-      <p className="home-description">
-        ¡Bienvenido a mi rincón digital! Soy Jefrey Ternera, un apasionado desarrollador frontend con habilidades en tecnologías web modernas.
-        Explora mi portafolio para descubrir proyectos emocionantes y conocer más sobre mi enfoque para construir experiencias web excepcionales.
-      </p>
-      <div className="home-image">
-          <img src={myImage} alt="Mi Imagen" className="main-image" />
-        </div>
-
+      <div className="home-title-description-div">
+  <h1 className="home-title">Hola, Me llamo Jefrey...</h1>
+  <p className="home-description">
+    ¡Bienvenido a mi rincón digital! Soy un apasionado desarrollador frontend con habilidades en tecnologías web modernas. Explora mi portafolio para descubrir proyectos emocionantes y conocer más sobre mi enfoque para construir experiencias web excepcionales.
+  </p>
+</div>
+<div className="home-image">
+  <img src={mainImage} alt="Mi Imagen" className="main-image" />
+</div>
+     
       <div className="tech-icons">
         <FontAwesomeIcon icon={faHtml5} size="3x" className="icon" />
         <FontAwesomeIcon icon={faCss3} size="3x" className="icon" />
